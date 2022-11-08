450570429f9d4606484df9f71c1f5c88b7961edb
Syd4r <72267789+Syd4r@users.noreply.github.com>
2022-11-07 19:53:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
     $q = $_REQUEST["q"];
-    $command = escapeshellcmd('py create_image.py '+ $q);
+    $xml = simplexml_load_string($xmlResponse);
+    $command = escapeshellcmd('py create_image.py '+ $xml);
     $output = shell_exec($command);
     echo true;
 ?>
